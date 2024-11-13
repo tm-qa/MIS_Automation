c2d5aa4ee9ffdf88ce8fc4a2b9e4f10ed983a6be
Turtlemint-QA Team <64896322+tm-qa@users.noreply.github.com>
2024-11-13 15:22:40 +0530

    mis Repo

--- src/main/java/com/qa/turtlemint/base/TestBase.java
+++ src/main/java/com/qa/turtlemint/base/TestBase.java
@@ -38,15 +38,11 @@ public class TestBase {
         String browserName = prop.getProperty("browser");
         LogUtils.info("Browser name: " + browserName);
         if (browserName.equals("chrome")) {
-            WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
-            options.setBrowserVersion("114");
             options.addArguments("--remote-allow-origins=*");
-            options.addExtensions();
 
 //            options.setBrowserVersion("124");
 //            driver = new ChromeDriver(options);
-
 
 
             String osName = System.getProperty("os.name");
@@ -67,9 +63,7 @@ public class TestBase {
 //				WebDriver driver = new ChromeDriver(options);
 
             }
-            WebDriverManager.chromedriver().setup();
                      driver = new ChromeDriver(options);
-                     options.setBrowserVersion("114");
 
             Dimension newDimension = new Dimension(1200, 800);
             driver.manage().window().setSize(newDimension);
