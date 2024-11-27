6c69e0a092b38ea03663a83afc670cf6cc41544f
Turtlemint-QA Team <64896322+tm-qa@users.noreply.github.com>
2024-11-27 10:50:46 +0530

    09 21 Manual Autoparse

--- src/test/java/Manual_Autoparse/TC_09_TO_21_Test.java
+++ src/test/java/Manual_Autoparse/TC_09_TO_21_Test.java
@@ -3,8 +3,12 @@ package Manual_Autoparse;
 import com.qa.turtlemint.base.TestBase;
 import com.qa.turtlemint.pages.login.LoginPage;
 import com.qa.turtlemint.pages.manualautoparse.TC_09_TO_21_Page;
+import com.qa.turtlemint.util.iTestListener;
 import org.testng.annotations.*;
 
+
+@Listeners(iTestListener.class)
+@Test(groups = {"Mis_Full", "TC_09_TO_21_Test"})
 public class TC_09_TO_21_Test extends TestBase {
     public LoginPage ninjaloginpage;
     public TC_09_TO_21_Page TC_09_TO_21_Page;
