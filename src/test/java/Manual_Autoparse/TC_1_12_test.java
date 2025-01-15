bccbab64028ddb10045f4e2a4978e110c9e45ce5
Sayali Dange <sayali.dange@turtlemint.com>
2025-01-15 10:52:31 +0530

    dashboard

--- src/test/java/Manual_Autoparse/TC_1_12_test.java
+++ src/test/java/Manual_Autoparse/TC_1_12_test.java
@@ -45,93 +45,43 @@ public class TC_1_12_test extends TestBase {
     @Test()
     public void TC3() throws Exception {
         ninjaloginpage.ninja_MIS();
-        create.Motor_productCatagory_Status_insurer("TW","Issued","HDFC Ergo","TW");
-        String misID = junkpolicyl.policyCkicked.getAttribute("value");
-        System.out.println(misID);
-        TC.basicDetails("Website","New","09-10-2024");
-        TC.saleDetails("09-10-2024");
-        TC.policyDetail();
+        create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
 
     }
     @Test()
     public void TC5() throws Exception {
         ninjaloginpage.ninja_MIS();
         create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
-        String misID = junkpolicyl.policyCkicked.getAttribute("value");
-        System.out.println(misID);
-        TC.basicDetails("Website","New","09-10-2024");
-        TC.saleDetails("09-10-2024");
-        TC.policyDetail();
 
     }
     @Test()
     public void TC6() throws Exception {
         ninjaloginpage.ninja_MIS();
         create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
-        String misID = junkpolicyl.policyCkicked.getAttribute("value");
-        System.out.println(misID);
-        TC.basicDetails("Website","New","09-10-2024");
-        TC.saleDetails("09-10-2024");
-        TC.policyDetail();
     }
     @Test()
     public void TC7() throws Exception {
 
         ninjaloginpage.ninja_MIS();
         create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
-        String misID = junkpolicyl.policyCkicked.getAttribute("value");
-        System.out.println(misID);
-        TC.basicDetails("Website","New","09-10-2024");
-        TC.saleDetails("09-10-2024");
-        TC.policyDetail();
     }
     public void TC8() throws Exception {
 
         ninjaloginpage.ninja_MIS();
         create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
-        String misID = junkpolicyl.policyCkicked.getAttribute("value");
-        System.out.println(misID);
-        TC.basicDetails("Website","New","09-10-2024");
-        TC.saleDetails("09-10-2024");
-        TC.policyDetail();
     }
 
     @Test()
     public void TC9() throws Exception {
 
         ninjaloginpage.ninja_MIS();
-        create.Motor_productCatagory_Status_insurer("TW","Pending from TM","Reliance","TW");
-        TC.generalDetails("New","11-10-2024");
-        TC.proposerDetails("Mr");
-        TC.vehicleDetails("OD");
-        TC.vehicleDetailsPCV("SCHOOL_BUS","Public");
-        TC.makeModel();
-        TC.variant();
-        TC.vehicleDetails1();
-        TC.dateEndorsementDtailsod("23-10-2024","23-10-2025");
-//        TC.premiumDetails();
-//        TC.qcDtails("Ready");
-//        TC.saleDetailsmanual("11-10-2024");
-//        TC.policyDetailmanual();
+        create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
     }
     @Test()
     public void TC10() throws Exception {
 
         ninjaloginpage.ninja_MIS();
-        create.Motor_productCatagory_Status_insurer("TW","Pending from TM","Reliance","TW");
-
-//        TC.generalDetails("New","11-10-2024");
-//        TC.proposerDetails("Mr");
-//        TC.vehicleDetails("OD");
-//        TC.vehicleDetailsPCV("SCHOOL_BUS","Public");
-//        TC.makeModel();
-//        TC.variant();
-//        TC.vehicleDetails1();
-//        TC.dateEndorsementDtailsod("23-10-2024","23-10-2025");
-//        TC.premiumDetails();
-//        TC.qcDtails("Ready");
-//        TC.saleDetailsmanual("11-10-2024");
-//        TC.policyDetailmanual();
+        create.Motor_productCatagory_Status_insurer("Motor","Issued","Bajaj Allianz","Car");
     }
 
 
