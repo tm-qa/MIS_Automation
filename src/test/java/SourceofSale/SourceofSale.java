4c9a1c114423f5e7213bfea633fa94a568e13ec5
Sayali Dange <sayali.dange@turtlemint.com>
2025-06-09 16:33:33 +0530

    Source_of_Sale

--- src/test/java/SourceofSale/SourceofSale.java
+++ src/test/java/SourceofSale/SourceofSale.java
@@ -13,6 +13,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+@Test(groups = {"Mis_Full","Source_of_Sale"})
 public class SourceofSale extends TestBase {
 
 
